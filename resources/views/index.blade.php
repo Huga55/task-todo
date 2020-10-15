b9e7380921e98c564e6b5c43d93c8529defd388f
huga55 <xp_92@mail.ru>
2020-10-15 22:46:26 +0500

    delete style standart

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
-	<link href="{{asset('css/app2.css')}}" rel="stylesheet" type="text/css">
 	<title>Document</title>
 </head>
 <body>
